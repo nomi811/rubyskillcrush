62af0df5bfa80d2cc808ae6bd1cd5ba5ce0f2013
Nomi Vos <nvoswork@gmail.com>
2018-08-22 17:08:39 -0500

    new challenge

---
+++ ruby_challenges/case.rb
@@ -0,0 +1,12 @@
+weather = 'sunny'
+
+case weather
+when 'sunny'
+  puts 'You can wear shorts and short sleeved shirt'
+when 'cloudy'
+  puts 'You better put on a long sleeved shirt today'
+when 'foggy'
+  puts 'You should wear a jacket today'
+when 'rainy'
+  puts 'Make sure you bring an umbrella today'
+end
