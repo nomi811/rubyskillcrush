1f175c7c624dde93cfbc15b5da87e059066237cf
Nomi Vos <nvoswork@gmail.com>
2018-08-24 15:39:03 -0500

    finishes numerology challenge and refractoring challenge

--- ruby_challenges/always_three.rb
+++ ruby_challenges/always_three.rb
@@ -1,6 +1,10 @@
+# puts "Give me a number:"
+# number1 = gets.chomp
+# number1 = number1.to_i
+# final_number = (((number1 + 5) * 2) - 4) /2
+# final_number -= number1
+# puts "The answer is always #{final_number}"
+
 puts "Give me a number:"
-number1 = gets.chomp
-number1 = number1.to_i
-final_number = (((number1 + 5) * 2) - 4) /2
-final_number -= number1
-puts "The answer is always #{final_number}"
+num = gets.chomp.to_i
+puts "The answer is always #{-num -= (((num + 5) *2) - 4) /2}."
